ab6b29831cfde620b6d330fe00c21f8d163ce312
Daniel <dnl.canedo@gmail.com>
2019-12-17 13:54:25 -0400

    Add Webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 const path = require('path');
+const _ = require('lodash');
+const outputDir = 'dist';
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'designer.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'Designer',
+const devConfig = {
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, outputDir),
+    
   },
-  devtool: 'inline-source-map',
+};
+
+const prodConfig = {
+  devtool: '(none)'
+};
+
+
+module.exports = (env, argv) => {
+  const mode = argv.mode;
+
+  return _.merge({
+    entry: './src/index.js',
+    output: {
+      filename: 'designer.js',
+      path: path.resolve(__dirname, outputDir),
+      library: 'Designer',
+    },
+  }, mode === 'production' ? prodConfig : devConfig);
 };
