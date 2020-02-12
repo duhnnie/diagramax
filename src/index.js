3be3fa7179a2b5442ab3e589c91e8073972ae1eb
Daniel <dnl.canedo@gmail.com>
2020-02-12 01:20:19 -0400

    DS-12 Add Circle shape for test connections and resizing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Canvas from './canvas/Canvas';
 import Box from './shape/Box';
+import Circle from './shape/Circle';
 
 export { Canvas };
 export { Box };
+export { Circle };
---
+++ src/shape/Circle.js
@@ -0,0 +1,40 @@
+import Element from '../core/Element';
+import Shape from './Shape';
+
+const DEFAULTS = Object.freeze({
+  width: 60,
+  height: 60,
+});
+
+class Circle extends Shape {
+  constructor(settings) {
+    settings = {
+      ...DEFAULTS,
+      ...settings,
+    };
+
+    super(settings);
+  }
+
+  _createHTML() {
+    if (!this._html) {
+      const circle = Element.createSVG('circle');
+
+      circle.setAttribute('cx', 0);
+      circle.setAttribute('cy', 0);
+      circle.setAttribute('r', this._width * 0.5);
+      circle.setAttribute('fill', '#ffffff');
+      circle.setAttribute('stroke', '#000000');
+      circle.setAttribute('stroke-width', '4');
+
+      super._createHTML();
+
+      this._dom.shapeElement = circle;
+      this._html.insertBefore(circle, this._dom.title);
+    }
+
+    return this;
+  }
+}
+
+export default Circle;
