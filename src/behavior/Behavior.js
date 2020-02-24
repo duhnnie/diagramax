4c97ac03c825b4ee447620c151f871607c7c3455
Daniel <dnl.canedo@gmail.com>
2020-02-24 09:57:13 -0400

    DS-30 Use this FunctionProxy  in a new method for attaching listeners in Behavior.js and save a reference to it for removing the listener in the detachBehavior() method V2.

--- src/behavior/Behavior.js
+++ src/behavior/Behavior.js
@@ -5,76 +5,8 @@ const DEFAULTS = Object.freeze({
   disabled: false,
 });
 
-const _listenersMap = new Map();
-
-function initListeners(instance) {
-  _listenersMap.set(instance, new WeakMap());
-}
-
-function getListeners(instance) {
-  return _listenersMap.get(instance);
-}
-
-function getHandlerArray(instance, source, event, handler) {
-  const listeners = getListeners(instance);
-  const sourceMap = listeners.get(source);
-
-  if (!sourceMap) return null;
-
-  const eventMap = sourceMap.get(event);
-
-  if (!eventMap) return null;
-
-  const handlerArray = eventMap.get(handler);
-
-  if (!handlerArray) return null;
-
-  return handlerArray;
-}
-
-function clearHandlerArray(instance, source, event, handler) {
-  const listeners = getListeners(instance);
-  const sourceMap = listeners.get(source);
-
-  if (!sourceMap) return false;
-
-  const eventMap = sourceMap.get(event);
-
-  if (!eventMap) return false;
-
-  return eventMap.delete(handler);
-}
-
-function setListener(instance, source, event, handler, proxied) {
-  const listeners = getListeners(instance);
-  let sourceMap = listeners.get(source);
-
-  if (!sourceMap) {
-    sourceMap = new Map();
-    listeners.set(source, sourceMap);
-  }
-
-  let eventMap = sourceMap.get(event);
-
-  if (!eventMap) {
-    eventMap = new WeakMap();
-    sourceMap.set(event, eventMap);
-  }
-
-  let handlerArray = eventMap.get(handler);
-
-  if (!handlerArray) {
-    handlerArray = [];
-    eventMap.set(handler, handlerArray);
-  }
-
-  handlerArray.push(proxied);
-}
-
 class Behavior {
   constructor(target, settings) {
-    initListeners(this);
-
     this._target = target;
 
     settings = _.merge({}, DEFAULTS, settings);
@@ -84,8 +16,23 @@ class Behavior {
     } else {
       this.enable();
     }
+  }
 
-    this._enabledEvaluator = this._enabledEvaluator.bind(this);
+  _enabledEvaluator() {
+    return !this._disabled;
+  }
+
+  /**
+   * Proxies a function to execute it or not depending on the state of the behavior
+   * (enabled/disabled). Bind the necessary methods to avoid the behavior to be executed.
+   * Even if a Behavior has more than one method that make the behavior to perform, the majority of
+   * times it will be enough by proxy just one method.
+   * @protected
+   * @param {Function} handler The function to be proxied.
+   * @returns {Function} The proxied funciton.
+   */
+  _bind(handler) {
+    return FunctionProxy.get(handler, '_enabledEvaluator', this);
   }
 
   disable() {
@@ -96,24 +43,6 @@ class Behavior {
     this._disabled = false;
   }
 
-  _enabledEvaluator() {
-    return !this._disabled;
-  }
-
-  _listenTo(source, event, handler) {
-    const functionProxy = FunctionProxy.get(handler, this._enabledEvaluator, this._target);
-
-    setListener(this, source, event, handler, functionProxy);
-    source.addEventListener(event, functionProxy, false);
-  }
-
-  _unlistenTo(source, event, handler) {
-    const handlers = getHandlerArray(this, source, event, handler) || [];
-
-    handlers.forEach((listener) => source.removeEventListener(event, listener));
-    clearHandlerArray(this, source, event, handler);
-  }
-
   // eslint-disable-next-line class-methods-use-this
   attachBehavior() {
     throw new Error('attachBehavior(): This method should be implemented');
@@ -121,7 +50,7 @@ class Behavior {
 
   // eslint-disable-next-line class-methods-use-this
   detachBehavior() {
-    throw new Error('detachBehavior(): This method should be implemented');
+    throw new Error('attachBehavior(): This method should be implemented');
   }
 }
 
