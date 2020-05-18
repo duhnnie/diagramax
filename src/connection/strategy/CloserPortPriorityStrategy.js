ce834bace168ca2cef294ed89857d6509af4e3d2
Daniel <dnl.canedo@gmail.com>
2020-05-18 01:00:01 -0400

    DS-113 Now CloserPortPriorityStrategy accepts Port as parameter too

--- src/connection/strategy/CloserPortPriorityStrategy.js
+++ src/connection/strategy/CloserPortPriorityStrategy.js
@@ -1,5 +1,6 @@
 import Geometry from '../../utils/Geometry';
 import Port, { ORIENTATION as PORT_ORIENTATION, MODE as PORT_MODE } from '../Port';
+import Shape from '../../shape/Shape';
 
 /**
  * Returns and array with the port indexes sorted in priority order for elegibility based on a
@@ -25,28 +26,38 @@ function getPortPriorityOrder(mainOrientation, relativeX, relativeY) {
 
 /**
  * Returns the ports in an array ordered by priority for each origin and destination shapes.
- * @param {Shape} origShape origin shape.
- * @param {Shape} destShape destination shape.
+ * @param {Shape|Port} orig origin shape.
+ * @param {Shape|Port} dest destination shape.
  * @returns {Object}
  */
-const CloserPortPriorityStrategy = function CloserPortPriorityStrategy(origShape, destShape) {
-  const origBounds = origShape.getBounds();
-  const destBounds = destShape.getBounds();
-  const origPosition = origShape.getPosition();
-  const destPosition = destShape.getPosition();
-  const { x: overlapX, y: overlapY } = Geometry.getOverlappedDimensions(origBounds, destBounds);
+const CloserPortPriorityStrategy = function CloserPortPriorityStrategy(orig, dest) {
+  if (orig instanceof Port && dest instanceof Port) return { orig, dest };
+
+  const origIsShape = orig instanceof Shape;
+  const destIsShape = dest instanceof Shape;
+  const origPosition = origIsShape ? orig.getPosition() : orig.getDescription().point;
+  const destPosition = destIsShape ? dest.getPosition() : dest.getDescription().point;
   const { x: relativeX, y: relativeY } = Geometry.getNormalizedPosition(origPosition, destPosition);
+  let overlapX = false;
+  let overlapY = false;
   let origPorts;
   let destPorts;
 
-  if (overlapX === overlapY) {
-    if (overlapX) {
-      origPorts = getPortPriorityOrder(PORT_ORIENTATION.X, relativeX, relativeY);
-      destPorts = getPortPriorityOrder(PORT_ORIENTATION.Y, relativeX, relativeY);
-    } else {
-      origPorts = getPortPriorityOrder(PORT_ORIENTATION.Y, relativeX, relativeY);
-      destPorts = getPortPriorityOrder(PORT_ORIENTATION.X, relativeX * -1, relativeY * -1);
-    }
+  if (origIsShape && destIsShape) {
+    const origBounds = orig.getBounds();
+    const destBounds = dest.getBounds();
+    const overlap = Geometry.getOverlappedDimensions(origBounds, destBounds);
+
+    overlapX = overlap.x;
+    overlapY = overlap.y;
+  }
+
+  if (overlapX && overlapY) {
+    origPorts = getPortPriorityOrder(PORT_ORIENTATION.X, relativeX, relativeY);
+    destPorts = getPortPriorityOrder(PORT_ORIENTATION.Y, relativeX, relativeY);
+  } else if (overlapX === overlapY) {
+    origPorts = getPortPriorityOrder(PORT_ORIENTATION.Y, relativeX, relativeY);
+    destPorts = getPortPriorityOrder(PORT_ORIENTATION.X, relativeX * -1, relativeY * -1);
   } else {
     let orientation;
 
@@ -62,11 +73,11 @@ const CloserPortPriorityStrategy = function CloserPortPriorityStrategy(origShape
     destPorts = getPortPriorityOrder(orientation, relativeX * -1, relativeY * -1);
   }
 
-  const origPort = origPorts.find((portIndex) => origShape.hasAvailablePortFor(portIndex, PORT_MODE.OUT));
+  const origPort = origPorts.find((portIndex) => orig.hasAvailablePortFor(portIndex, PORT_MODE.OUT));
   const destPort = destPorts.find((portIndex) => {
-    if (origShape === destShape && portIndex === origPort) return false;
+    if (orig === dest && portIndex === origPort) return false;
 
-    return destShape.hasAvailablePortFor(portIndex, PORT_MODE.IN);
+    return dest.hasAvailablePortFor(portIndex, PORT_MODE.IN);
   });
 
   return {
