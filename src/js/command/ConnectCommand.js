77598a9464eba1a20a8389c085ad23b9494e927a
Daniel <dnl.canedo@gmail.com>
2020-06-13 01:38:31 -0400

    DS-94 ConnectCommand also works for reconnection

--- src/js/command/ConnectCommand.js
+++ src/js/command/ConnectCommand.js
@@ -4,21 +4,33 @@ class ConnectCommand extends Command {
   constructor(receiver, origShape, destShape, connection) {
     super(receiver);
 
+    this._before = {
+      origShape: connection ? connection.getOrigShape() : null,
+      destShape: connection ? connection.getDestShape() : null,
+    };
+
     this._after = {
       origShape,
       destShape,
-      connection,
     };
+
+    this._connection = connection;
   }
 
   execute() {
-    const { origShape, destShape, connection = null } = this._after;
+    const { origShape, destShape } = this._after;
 
-    this._after.connection = this._receiver.connect(origShape, destShape, connection);
+    this._connection = this._receiver.connect(origShape, destShape, this._connection);
   }
 
   undo() {
-    this._after.connection.remove();
+    const { origShape, destShape } = this._before;
+
+    if (origShape && destShape) {
+      this._receiver.connect(origShape, destShape, this._connection);
+    } else {
+      this._connection.remove();
+    }
   }
 }
 
